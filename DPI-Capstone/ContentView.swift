7069c149248ae2d1639c0cf3298c6962cb720d18
Alina Liu <alinaliu548@gmail.com>
2026-07-07 10:38:17 -0500

    Initial Commit

---
+++ DPI-Capstone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DPI-Capstone
+//
+//  Created by DPI Student 009 on 7/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DPI-Capstone/DPI_CapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  DPI_CapstoneApp.swift
+//  DPI-Capstone
+//
+//  Created by DPI Student 009 on 7/7/26.
+//
+
+import SwiftUI
+
+@main
+struct DPI_CapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
