84295782691a291ecebe3198a9394d886807471f
Alina L. <alinaliu548@gmail.com>
2026-07-07 11:14:21 -0500

    Test

---
+++ DPI-Capstone/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  DPI-Capstone
+//
+//  Created by DPI Student 009 on 7/7/26.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
