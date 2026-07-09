e089144622598984f51148a9de3be6d82499e930
Alina L. <alinaliu548@gmail.com>
2026-07-09 10:51:21 -0500

    added opportunity library

---
+++ DPI-Capstone/Models/OpportunitiesLibrary.swift
@@ -0,0 +1,59 @@
+//
+//  OpportunitiesLibrary.swift
+//  DPI-Capstone
+//
+//  Created by DPI Student 009 on 7/9/26.
+//
+
+import Foundation
+
+let Opportunities: [Opportunity] = [
+    Opportunity(lat: <#T##Double#>,
+                long: <#T##Double#>,
+                interestTags: <#T##[Interests]#>,
+                organization: <#T##String#>,
+                description: <#T##String#>,
+                date: <#T##Date#>),
+    Opportunity(lat: <#T##Double#>,
+                long: <#T##Double#>,
+                interestTags: <#T##[Interests]#>,
+                organization: <#T##String#>,
+                description: <#T##String#>,
+                date: <#T##Date#>),
+    Opportunity(lat: 132.143,
+                long: <#T##Double#>,
+                interestTags: <#T##[Interests]#>,
+                organization: <#T##String#>,
+                description: <#T##String#>,
+                date: <#T##Date#>),
+    Opportunity(lat: <#T##Double#>,
+                long: <#T##Double#>,
+                interestTags: <#T##[Interests]#>,
+                organization: <#T##String#>,
+                description: <#T##String#>,
+                date: <#T##Date#>),
+    Opportunity(lat: <#T##Double#>,
+                long: <#T##Double#>,
+                interestTags: <#T##[Interests]#>,
+                organization: <#T##String#>,
+                description: <#T##String#>,
+                date: <#T##Date#>),
+    Opportunity(lat: <#T##Double#>,
+                long: <#T##Double#>,
+                interestTags: <#T##[Interests]#>,
+                organization: <#T##String#>,
+                description: <#T##String#>,
+                date: <#T##Date#>),
+    Opportunity(lat: <#T##Double#>,
+                long: <#T##Double#>,
+                interestTags: <#T##[Interests]#>,
+                organization: <#T##String#>,
+                description: <#T##String#>,
+                date: <#T##Date#>),
+    Opportunity(lat: <#T##Double#>,
+                long: <#T##Double#>,
+                interestTags: <#T##[Interests]#>,
+                organization: <#T##String#>,
+                description: <#T##String#>,
+                date: <#T##Date#>),
+        ]
