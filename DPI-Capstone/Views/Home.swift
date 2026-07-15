611eb5cd652e656de522ac12a867ee1cb769b2b4
Alina L. <alinaliu548@gmail.com>
2026-07-15 13:27:22 -0500

    Minor changes to home view

---
+++ DPI-Capstone/Views/Home.swift
@@ -0,0 +1,33 @@
+//
+//  Onboarding.swift
+//  DPI-Capstone
+//
+//  Created by DPI Student 009 on 7/7/26.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        ZStack{
+            Color.offWhite
+                .ignoresSafeArea()
+            VStack{
+                Text("Opportunities Near You")
+                    .font(.system(size: 32, weight: .bold))
+                    .foregroundStyle(Color.darkerGreen)
+                    .padding(.horizontal)
+                
+                HStack{
+                    
+                }
+            }
+        }
+        
+      
+    }
+}
+
+#Preview {
+    Home()
+}
