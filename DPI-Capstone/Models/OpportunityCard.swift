3e235d07d2b21cec489b71ebf061659395549b27
Alina L. <alinaliu548@gmail.com>
2026-07-15 13:34:04 -0500

    started on opportunityCard struct

---
+++ DPI-Capstone/Models/OpportunityCard.swift
@@ -0,0 +1,25 @@
+//
+//  OpportunityCard.swift
+//  DPI-Capstone
+//
+//  Created by DPI Student 009 on 7/15/26.
+//
+
+import SwiftUI
+
+struct OpportunityCard: View {
+    
+    var opportunity: Opportunity
+    
+    var body: some View {
+        VStack{
+            
+        }
+        Color.offWhite
+            .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    OpportunityCard(opportunity: Opportunities[0])
+}
